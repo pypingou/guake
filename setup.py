40bf44e52da234723da71ab160c78276d0f57f02
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-04-04 15:24:46 +0200

    Update setup.py
    - install the data files in the correct way rather than using an external shell script
    - update the template files at install
    
    This make guake usuable within a virtualenv

--- setup.py
+++ setup.py
@@ -15,14 +15,53 @@ PO_DIR = 'po'
 MO_DIR = os.path.join('build', 'mo')
 
 
+def update_file_variable(filein, fileout, dict_var):
+    """ Reads in the filein, replace the variable set as key of the dict_var
+    by their values provided in the same dict_var and save the whole into
+    fileout."""
+    try:
+        stream = open(filein)
+        content = stream.read()
+        stream.close()
+    except IOError, err:
+        print 'Could not read file %s' % filein
+        print 'ERROR: %s' % err.message
+        return
+
+    # replace the variables
+    for key in dict_var:
+        content = content.replace(key, dict_var[key])
+
+    try:
+        stream = open(fileout, 'w')
+        stream.write(content)
+        stream.close()
+    except IOError, err:
+        print 'Could not write file %s' % fileout
+        print 'ERROR: %s' % err.message
+        return
+
+
 class InstallAndUpdateDataDirectory(install):
 
     def run(self):
+
+        dict_var = {'@VERSION@': '0.5.0',
+                    '@DATADIR@': '%s/share' % self.prefix,
+                    '@BINDIR@': '%s/bin' % self.prefix}
+
+        # Fill template dbus service
+        update_file_variable('data/org.guake.Guake.service.in',
+                             'data/org.guake.Guake.service',
+                             dict_var)
+
+        # Fill template config variable
+        update_file_variable('src/globals.py.in',
+                             'src/globals.py',
+                             dict_var)
+
         # Run parent install method
         install.run(self)
-
-        # Run shell script to install data files
-        get_command_output('sh ./install_data.sh %s' % self.prefix)
 
         # Compile and install languages
         for po in glob.glob (os.path.join (PO_DIR, '*.po')):
@@ -109,6 +148,25 @@ again to hide.''',
     package_dir = {'guake': 'src'},
     packages = ['guake'],
     scripts=['src/guake'],
+    data_files = [
+        ('share/pixmaps/guake', glob.glob('data/pixmaps/*')),
+        ('share/man/man1', ['data/guake.1']),
+        ('share/guake', glob.glob('data/*.glade')),
+        ('share/dbus-1/services/', ['data/org.guake.Guake.service']),
+        ('share/application/', glob.glob('data/*.desktop')),
+        ('share/icons/hicolor/16x16/apps/',
+         glob.glob('data/icons/16x16/apps/*.png')),
+        ('share/icons/hicolor/22x22/apps/',
+         glob.glob('data/icons/22x22/apps/*.png')),
+        ('share/icons/hicolor/24x24/apps/',
+         glob.glob('data/icons/24x24/apps/*.png')),
+        ('share/icons/hicolor/32x32/apps/',
+         glob.glob('data/icons/32x32/apps/*.png')),
+        ('share/icons/hicolor/48x48/apps/',
+         glob.glob('data/icons/48x48/apps/*.png')),
+        ('share/icons/hicolor/256x256/apps/',
+         glob.glob('data/icons/256x256/apps/*.png')),
+        ],
     cmdclass={'install': InstallAndUpdateDataDirectory},
-    ext_modules=[globalhotkeys]
+    ext_modules=[globalhotkeys],
     )
